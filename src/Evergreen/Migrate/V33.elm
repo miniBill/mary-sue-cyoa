7a1e50aa659d14b4b2ad1cf709498207a1b239e9
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-02-04 17:42:49 +0100

    Prepare for new versions

--- src/Evergreen/Migrate/V33.elm
+++ src/Evergreen/Migrate/V33.elm
@@ -186,7 +186,7 @@ migrate_Types_Password_Password : Evergreen.V31.Types.Password.Password -> Everg
 migrate_Types_Password_Password old =
     case old of
         Evergreen.V31.Types.Password.Password p0 ->
-            Evergreen.V33.Types.Password.password p0
+            Evergreen.V33.Types.Password.Password p0
 
 
 migrate_Types_Tier : Evergreen.V31.Types.Tier -> Evergreen.V33.Types.Tier
